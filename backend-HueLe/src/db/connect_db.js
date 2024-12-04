e59de060b1993cc90f25e431f33f0977429155fd
Huệ Lê <125109347+huele1802@users.noreply.github.com>
2024-12-04 15:58:55 +0700

    Update connect_db.js

--- backend-HueLe/src/db/connect_db.js
+++ backend-HueLe/src/db/connect_db.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 
 async function connect() {
   try {
-    // await mongoose.connect(process.env.AtlasURI, {
-    //   family: 4,
-    // });
-    await mongoose.connect(process.env.MongoURI,{
-        useNewUrlParser: true, 
-        useUnifiedTopology: true,
-        family: 4,
-    })
+    await mongoose.connect(process.env.AtlasURI, {
+      family: 4,
+    });
+    // await mongoose.connect(process.env.MongoURI,{
+    //     useNewUrlParser: true, 
+    //     useUnifiedTopology: true,
+    //     family: 4,
+    // })
     console.log("Database connection successful");
   } catch (error) {
     console.log("Database connection failed: ", error);
